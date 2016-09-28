633bf3b5bd5acc23fbbf5cc5a3814024619792a2
DarthWeirdo <atot1981@gmail.com>
2016-09-28 16:22:04 +0200

    First commit

---
+++ GameOfLife/AdWindow.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+namespace GameOfLife
+{
+    internal class AdWindow : Window
+    {
+        private readonly DispatcherTimer _adTimer;
+        private int _imgNmb;     // the number of the image currently shown
+        private string _link;    // the URL where the currently shown ad leads to
+        
+    
+        public AdWindow(Window owner)
+        {
+            var rnd = new Random();
+            Owner = owner;
+            Width = 350;
+            Height = 100;
+            ResizeMode = ResizeMode.NoResize;
+            WindowStyle = WindowStyle.ToolWindow;
+            Title = "Support us by clicking the ads";
+            Cursor = Cursors.Hand;
+            ShowActivated = false;
+            MouseDown += OnClick;
+            
+            _imgNmb = rnd.Next(1, 3);
+            ChangeAds(this, new EventArgs());
+
+            // Run the timer that changes the ad's image 
+            _adTimer = new DispatcherTimer {Interval = TimeSpan.FromSeconds(3)};
+            _adTimer.Tick += ChangeAds;
+            _adTimer.Start();
+        }
+
+        private void OnClick(object sender, MouseButtonEventArgs e)
+        {
+            System.Diagnostics.Process.Start(_link);
+            Close();
+        }
+        
+        protected override void OnClosed(EventArgs e)
+        {
+//            Unsubscribe();
+            base.OnClosed(e);
+        } 
+
+        public void Unsubscribe()
+        {
+            _adTimer.Tick -= ChangeAds;            
+        }
+
+        private void ChangeAds(object sender, EventArgs eventArgs)
+        {
+            
+            var myBrush = new ImageBrush();
+            
+            switch (_imgNmb)
+            {
+                case 1:
+                    myBrush.ImageSource =
+                        new BitmapImage(new Uri("ad1.png", UriKind.Relative));
+                    Background = myBrush;
+                    _link = "http://example.com";
+                    _imgNmb++;
+                    break;
+                case 2:
+                    myBrush.ImageSource =
+                        new BitmapImage(new Uri("ad2.png", UriKind.Relative));
+                    Background = myBrush;
+                    _link = "http://example.com";
+                    _imgNmb++;
+                    break;
+                case 3:
+                    myBrush.ImageSource =
+                        new BitmapImage(new Uri("ad3.png", UriKind.Relative));
+                    Background = myBrush;
+                    _link = "http://example.com";
+                    _imgNmb = 1;
+                    break;
+            }
+            
+        }
+    }
+}
---
+++ GameOfLife/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace GameOfLife
+{
+    public partial class MainWindow
+    {
+        private readonly Grid _mainGrid;
+        private readonly DispatcherTimer _timer; //  Generation timer
+        private int _genCounter;
+        private AdWindow[] _adWindow;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _mainGrid = new Grid(MainCanvas);
+
+            _timer = new DispatcherTimer();
+            _timer.Tick += OnTimer;
+            _timer.Interval = TimeSpan.FromMilliseconds(200);
+        }
+
+
+        private void StartAd()
+        {
+            _adWindow = new AdWindow[2];
+            for (var i = 0; i < 2; i++)
+            {
+                if (_adWindow[i] != null) continue;
+                _adWindow[i] = new AdWindow(this);
+                _adWindow[i].Closed += AdWindowOnClosed;
+                _adWindow[i].Top = this.Top + (330*i) + 70;
+                _adWindow[i].Left = this.Left + 240;
+                _adWindow[i].Show();
+            }
+        }
+
+        private void AdWindowOnClosed(object sender, EventArgs eventArgs)
+        {
+            for (var i = 0; i < 2; i++)
+            {
+                _adWindow[i].Closed -= AdWindowOnClosed;
+                _adWindow[i] = null;
+            }
+        }
+
+
+        private void Button_OnClick(object sender, EventArgs e)
+        {
+            if (!_timer.IsEnabled)
+            {
+                _timer.Start();
+                ButtonStart.Content = "Stop";
+                StartAd();
+            }
+            else
+            {
+                _timer.Stop();
+                ButtonStart.Content = "Start";
+            }
+        }
+
+        private void OnTimer(object sender, EventArgs e)
+        {
+            _mainGrid.Update();
+            _genCounter++;
+            lblGenCount.Content = "Generations: " + _genCounter;
+        }
+
+        private void ButtonClear_Click(object sender, RoutedEventArgs e)
+        {
+            _mainGrid.Clear();
+        }
+    }
+}
